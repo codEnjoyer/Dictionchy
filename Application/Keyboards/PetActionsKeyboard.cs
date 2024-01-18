7b2b5411022a575c4e3af1adaf295aaa0e1bb814
SomEnaMeforme <98802409+SomEnaMeforme@users.noreply.github.com>
2024-01-18 15:34:45 +0500

    Переход на другой тип клавиатуры

--- Application/Keyboards/PetActionsKeyboard.cs
+++ Application/Keyboards/PetActionsKeyboard.cs
@@ -4,9 +4,6 @@ namespace Dictionchy.Application.Keyboards
 {
     public class PetActionsKeyboard : Keyboard
     {
-        public override InlineKeyboardButton[] Buttons => new[]
-        {
-            InlineKeyboardButton.WithCallbackData(text: "Заглушка", callbackData: "/empty")
-        };
+        public override KeyboardButton[] Buttons => new KeyboardButton[] { "/empty" };
     }
 }
--- Application/Keyboards/PetKeyboard.cs
+++ Application/Keyboards/PetKeyboard.cs
@@ -4,10 +4,6 @@ namespace Dictionchy.Application.Keyboards
 {
     public class PetKeyboard : Keyboard
     {
-        public override InlineKeyboardButton[] Buttons => new[]
-        {
-            InlineKeyboardButton.WithCallbackData(text: "Посмотреть состояние питомца", callbackData: "/empty"), //petState || petInfo
-            InlineKeyboardButton.WithCallbackData(text: "Перейти к действиям с питомцем", callbackData: "/empty"), //petActions
-        };
+        public override KeyboardButton[] Buttons => new KeyboardButton[] { "/petActions", "/petInfo" };
     }
 }
