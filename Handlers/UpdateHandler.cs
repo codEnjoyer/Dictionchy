6cc3f5e0896224aeeb525b76982b01b9b077e191
SomEnaMeforme <98802409+SomEnaMeforme@users.noreply.github.com>
2024-01-18 16:27:35 +0500

    Update UpdateHandler.cs

--- Handlers/UpdateHandler.cs
+++ Handlers/UpdateHandler.cs
@@ -1,13 +1,7 @@
 ﻿using Dictionchy.Application.Commands;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types;
 using Telegram.Bot;
-using System.Threading;
 
 namespace Dictionchy.Handlers
 {
