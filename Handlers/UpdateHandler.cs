ece9cab0ee49f9dc35d1fec7e28750ca2601ceb3
SomEnaMeforme <98802409+SomEnaMeforme@users.noreply.github.com>
2024-01-26 17:25:10 +0500

    Переход на NotExistCommandException
    Перенос ответственности за обработку несуществующей команды на кастомную ошибку и обработчик ошибок

--- Handlers/UpdateHandler.cs
+++ Handlers/UpdateHandler.cs
@@ -7,7 +7,7 @@ namespace Dictionchy.Handlers
 {
     public static class UpdateHandler
     {
-        private static readonly CommandManager CommandManager = new();
+        private static readonly CommandManager CommandManager = new(); //TODO: добавить в контейнер
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
             CancellationToken cancellationToken)
         {
@@ -40,7 +40,8 @@ namespace Dictionchy.Handlers
 
             if (isCommand) 
             {
-                var commandResult = CommandManager.ExecuteCommand(commandName ?? "/empty", update);
+                
+                var commandResult = CommandManager.ExecuteCommand(commandName, update);
                 await botClient.SendTextMessageAsync(message!.Chat,
                     commandResult.Message,
                     replyMarkup: commandResult.ReplyKeyboard?.GetKeyboard());
@@ -53,7 +54,7 @@ namespace Dictionchy.Handlers
             CancellationToken cancellationToken)
         {
             var callbackQuery = update.CallbackQuery;
-            var callbackResult = CommandManager.ExecuteCommand(callbackQuery?.Data ?? "/empty");
+            var callbackResult = CommandManager.ExecuteCommand(callbackQuery?.Data);
             if (callbackQuery is {Message: not null})
             {
                 await botClient.SendTextMessageAsync(callbackQuery.Message.Chat,
